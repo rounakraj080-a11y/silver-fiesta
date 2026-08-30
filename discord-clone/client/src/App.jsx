917e0704ec6c53826bbe160e62c3b392f8bf163d
rounakraj080-a11y <rounakraj080@gmail.com>
2026-08-30 13:20:26 +0530

    Update App.jsx

--- discord-clone/client/src/App.jsx
+++ discord-clone/client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuth } from "./context/AuthContext.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
 import ChatApp from "./pages/ChatApp.jsx";
 
 function ProtectedRoute({ children }) {
@@ -23,6 +24,7 @@ export default function App() {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route
         path="/channels"
         element={
